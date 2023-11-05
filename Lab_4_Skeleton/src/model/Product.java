6f216a78d5d4e94fccff03e8efe3417315b56cae
advaithkrishna7 <vasisht.a@northeastern.edu>
2023-11-05 16:11:03 -0500

    Lab 4

---
+++ Lab_4_Skeleton/src/model/Product.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rushabh
+ */
+public class Product {
+    
+    private String name;
+    private int price;
+    private int id;
+
+    private static int count = 0;
+    
+    private ArrayList<Feature> features;
+    
+    public Product() {
+        count++;
+        id = count;
+        
+        features = new ArrayList<Feature>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Product.count = count;
+    }
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+    
+    public Feature addNewFeature(String name, String value) {
+        Feature f = new Feature(this);
+        features.add(f);
+        f.setName(name);
+        f.setValue(value);
+        return f;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
